cc9aa627b812ec806a106e3b37e9ae09b643e290
matejgerek <maftvej@gmail.com>
2023-03-21 15:46:44 +0100

    Add squadLabelsSet

--- .github/workflows/assignSquadLabel/index.js
+++ .github/workflows/assignSquadLabel/index.js
@@ -2,17 +2,18 @@ const core = require("@actions/core");
 const squadMapping = require('./squad-mapping.json');
 const { getOctokit, context } = require("@actions/github");
 
-const SQUADS = ["Squad 1", "Squad 2", "Squad 3"]
+const squadLabels = new Set(squadMapping.map(mapping => mapping.label));
 
 async function assignLabelsToIssue() {
   const assignees = context.payload.issue.assignees;
   const currentLabels = context.payload.issue.labels.map(label => label.name);
   const issueNumber = context.payload.issue.number;
+
   const { rest: client } = getOctokit(process.env.GITHUB_AUTH);
 
   const labelsByAssignees = assignees.flatMap(assignee => squadMapping.find(mapping => mapping.login === assignee.login)?.label ?? []);
 
-  const labelsToRemove = currentLabels.filter(label => SQUADS.includes(label) && !labelsByAssignees.includes(label));
+  const labelsToRemove = currentLabels.filter(label => squadLabels.has(label) && !labelsByAssignees.includes(label));
 
   labelsToRemove.forEach(async (label) => {
     await client.issues.removeLabel({
@@ -23,7 +24,7 @@ async function assignLabelsToIssue() {
     });
   });
 
-  const labelsToAdd = new Set(labelsByAssignees.filter(label => !currentLabels.includes(label)).concat(['Squad 2', 'Squad 2']));
+  const labelsToAdd = new Set(labelsByAssignees.filter(label => !currentLabels.includes(label)));
 
   if (labelsToAdd.size > 0) {
     await client.issues.addLabels({
