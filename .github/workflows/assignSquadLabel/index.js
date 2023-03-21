a8c7c10c9bb365b0aea5b40af22b2b08f2df82a1
matejgerek <maftvej@gmail.com>
2023-03-21 14:12:15 +0100

    Add whole functionality

--- .github/workflows/assignSquadLabel/index.js
+++ .github/workflows/assignSquadLabel/index.js
@@ -5,31 +5,42 @@ const {getOctokit, context} = require("@actions/github");
 
 const run = async () => {
   const assignees = context.payload.issue.assignees || [];
-
-  const currentLabels = context.payload.issue.labels || [];
-
-  console.log({currentLabels})
+  const currentLabels = (context.payload.issue.labels || []).map(label => label.name);
 
   const labelsByAssignees = assignees.map(assignee => {
     const squad = squadMapping.find(mapping => mapping.login === assignee.login);
     return squad ? squad.label : null;
   }).filter(label => label !== null);
 
-  console.log({labelsByAssignees})
-
-const issue_number = context.payload.issue.number;
-
-  console.log({issue_number})
+  const issue_number = context.payload.issue.number;
 
   const {rest: client} = getOctokit(process.env.GITHUB_AUTH);
 
-  await client.issues.addLabels({
+  const labelsToRemove = currentLabels.filter(label => {
+    const squad = squadMapping.find(mapping => mapping.label === label);
+    return squad && !labelsByAssignees.includes(label);
+  });
+
+  for (const label of labelsToRemove) {
+    await client.issues.removeLabel({
       owner: context.repo.owner,
       repo: context.repo.repo,
       issue_number,
-      labels: labelsByAssignees,
-  })
+      name: label,
+    });
+  }
 
-}
+  const labelsToAdd = labelsByAssignees.filter(label => !currentLabels.includes(label));
+
+  await client.issues.addLabels({
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    issue_number,
+    labels: labelsToAdd,
+  });
+};
+
+run();
+
 
 run();
