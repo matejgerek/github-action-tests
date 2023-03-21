dfbd43ea63f18fad338c8688af83f61184dd8e96
matejgerek <maftvej@gmail.com>
2023-03-21 12:43:28 +0100

    Add labels

--- .github/workflows/assignSquadLabel/index.js
+++ .github/workflows/assignSquadLabel/index.js
@@ -2,13 +2,24 @@ const core = require("@actions/core");
 const github = require("@actions/github");
 const squadMapping = require('./squad-mapping.json');
 
-const run = () => {
 
-    console.log({squadMapping})
+const run = async () => {
+  const assignees = github.context.payload.issue.assignees || [];
 
+  // Find corresponding labels for each assignee
+  const labels = assignees.map(assignee => {
+    const squad = squadMapping.find(mapping => mapping.login === assignee.login);
+    return squad ? squad.label : null;
+  }).filter(label => label !== null);
 
-    const assignees = github.context.payload.issue.assignees || [];
-    console.log(assignees)
+  // Add labels to the issue
+  const octokit = github.getOctokit(core.getInput('github-token'));
+  await octokit.issues.addLabels({
+    owner: github.context.repo.owner,
+    repo: github.context.repo.repo,
+    issue_number: github.context.payload.issue.number,
+    labels: labels
+  });
 }
 
 run();
