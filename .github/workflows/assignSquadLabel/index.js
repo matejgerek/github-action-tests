8fa70c7c898b7148a5c18626e80bdc62c2450e4a
matejgerek <maftvej@gmail.com>
2023-03-21 14:54:58 +0100

    Teest

--- .github/workflows/assignSquadLabel/index.js
+++ .github/workflows/assignSquadLabel/index.js
@@ -1,50 +1,40 @@
 const core = require("@actions/core");
 const squadMapping = require('./squad-mapping.json');
-const {getOctokit, context} = require("@actions/github");
+const { getOctokit, context } = require("@actions/github");
 
+async function assignLabelsToIssue() {
+  const assignees = context.payload.issue.assignees;
+  const currentLabels = context.payload.issue.labels.map(label => label.name);
+  const issueNumber = context.payload.issue.number;
+  const { rest: client } = getOctokit(process.env.GITHUB_AUTH);
 
-const run = async () => {
-  const assignees = context.payload.issue.assignees || [];
-  console.log(context.payload.issue)
-  const currentLabels = (context.payload.issue.labels || []).map(label => label.name);
-
-  const labelsByAssignees = assignees.map(assignee => {
-    const squad = squadMapping.find(mapping => mapping.login === assignee.login);
-    return squad ? squad.label : null;
-  }).filter(label => label !== null);
-
-  const issue_number = context.payload.issue.number;
-  console.log({labelsByAssignees})
-
-  const {rest: client} = getOctokit(process.env.GITHUB_AUTH);
+  const labelsByAssignees = assignees.flatMap(assignee => squadMapping.find(mapping => mapping.login === assignee.login)?.label ?? []);
 
   const labelsToRemove = currentLabels.filter(label => {
     const squad = squadMapping.find(mapping => mapping.label === label);
     return squad && !labelsByAssignees.includes(label);
   });
 
-  for (const label of labelsToRemove) {
+  labelsToRemove.forEach(async (label) => {
     await client.issues.removeLabel({
       owner: context.repo.owner,
       repo: context.repo.repo,
-      issue_number,
+      issue_number: issueNumber,
       name: label,
     });
-  }
+  });
 
-  const labelsToAdd = labelsByAssignees.filter(label => !currentLabels.includes(label));
+  const labelsToAdd = labelsByAssignees.filter(label => !currentLabels.includes(label)).concat(['Squad 2', 'Squad 2']);
 
-  if(labelsToAdd.length > 0) {
+  if (labelsToAdd.length > 0) {
     await client.issues.addLabels({
       owner: context.repo.owner,
       repo: context.repo.repo,
-      issue_number,
+      issue_number: issueNumber,
       labels: labelsToAdd,
     });
   }
-};
+}
 
-run();
-
-
-run();
+// Call the main function
+assignLabelsToIssue();
