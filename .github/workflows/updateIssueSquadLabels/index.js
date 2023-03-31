2ae94fb5b2daae903841dc85203799b48851c188
matejgerek <maftvej@gmail.com>
2023-03-31 18:45:07 +0200

    Fix current labels

--- .github/workflows/updateIssueSquadLabels/index.js
+++ .github/workflows/updateIssueSquadLabels/index.js
@@ -4,14 +4,14 @@ const { getOctokit, context } = require("@actions/github");
 
 const updateIssueSquadLabels = async () => {
   const assignees = context.payload.issue.assignees;
-  const currentLabels = context.payload.issue.labels.map(label => label.name);
+  const currentLabelsWithoutSquadLabels = context.payload.issue.labels.map(label => label.name).filter(label => !label.startsWith('Squad'));
   const issueNumber = context.payload.issue.number;
 
   const { rest: client } = getOctokit(process.env.GITHUB_AUTH);
 
   const labelsByAssignees = assignees.flatMap(assignee => squadMapping.find(mapping => mapping.login === assignee.login)?.label ?? []);
 
-  const updatedLabels = [...new Set([...currentLabels, ...labelsByAssignees])];
+  const updatedLabels = [...new Set([...currentLabelsWithoutSquadLabels, ...labelsByAssignees])];
 
   console.log({updatedLabels})
 
